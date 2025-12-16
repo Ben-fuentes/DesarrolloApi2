3103188ed46a23c8137d05b9bb7f996b902d3533
alan-flores1 <ala.floresr@duocuc.cl>
2025-12-15 23:17:48 -0300

    Refactor Producto class to use String for categoria
    Removed the @Enumerated annotation and the Categoria enum, changing categoria to a String.

--- src/main/java/com/example/miapp/model/Producto.java
+++ src/main/java/com/example/miapp/model/Producto.java
@@ -4,32 +4,25 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Data 
-@NoArgsConstructor 
-@AllArgsConstructor 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
 
-    @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nombre; 
-    private Double precio; 
+    private String nombre;
+    private Double precio;
     private String descripcion;
     private String img;
     private Integer stock;
     private boolean enOferta = false;
     private Integer precioAntes = null;
 
-    @Enumerated(EnumType.STRING)
-
-    private Categoria categoria;  
-    
-    public enum Categoria {
-        BMX,
-        ROLLER,
-        SKATE
-    };
-    
+    // CAMBIO IMPORTANTE:
+    // Borramos @Enumerated y el public enum.
+    // Ahora es simplemente un String.
+    private String categoria; 
 }
-
